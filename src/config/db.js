517e7d4e3cd19219f649aa486407efcb25119744
Patel9Sshubham <shubhampatel98937@gmail.com>
2025-01-29 17:36:12 +0530

    third

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,26 @@
 
-const Sequelize = require("sequelize");
-require("dotenv").config();
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
 
-const sequelize = new Sequelize( 
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: process.env.DB_DIALECT,
-    host: process.env.DB_HOST
-  }
-);
+// const sequelize = new Sequelize( 
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: process.env.DB_DIALECT,
+//     host: process.env.DB_HOST
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+});
 
 module.exports = sequelize;
